8b899a7f3c270c8fa50a41c3957ac7b19956f6da
Carlos Antônio <105462756+carlosaljuniorcg@users.noreply.github.com>
2023-02-06 23:10:38 -0300

    Update 06022023

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,24 @@
+import styles from './Favoritos.module.css';    
+import Titulo from 'componentes/Titulo';
+import Banner from 'componentes/Banner';
+import Card from 'componentes/Card';
+import { useFavoritoContext } from 'Contextos/Favoritos';
+
+function Favoritos() {
+    const { favorito } = useFavoritoContext();
+    return (
+        <>
+            <Banner imagem="favoritos" />
+                <Titulo>
+                    <h1>Meus Favoritos</h1>
+                </Titulo>
+                <section className={styles.container}>
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
+            </section>
+        </>
+    )
+}
+
+export default Favoritos;
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,33 @@
+import Banner from "componentes/Banner";
+import Card from "componentes/Card";
+import Titulo from "componentes/Titulo";
+import { useEffect, useState } from "react";
+import styles from './Inicio.module.css';
+
+
+function Inicio() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/carlosaljuniorcg/cinetag-api/videos')
+            .then(respostas =>respostas.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+    return (
+        <>
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus videos e filmes! </h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                   return <Card {...video} key={video.id}/>
+                })}
+            </section>
+        </>
+    )
+}
+
+export default Inicio;
